e82bbc43df8cb5f248576129c4ade74de425c2a1
hxnky <khk_1026@naver.com>
2020-09-28 14:51:57 +0900

    200928-1

---
+++ java_project/.metadata/.plugins/org.eclipse.core.resources/.history/16/901335724e01001b1dd2c4c68ce38822
@@ -0,0 +1,11 @@
+package first;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
--- java_project/FirstJava/src/first/FirstJava.java
+++ java_project/FirstJava/src/first/FirstJava.java
@@ -1,9 +1,15 @@
 package first;
+// first.FirstJava
 
 public class FirstJava {
 
 	public static void main(String[] args) {
+		// 제어자(접근) 제어자 반환형타입 메서드(기능)
 		
+		System.out.println("Hello Java!"); // 클래스의 이름은 대문자로 시작해야함
+		
+		
+	
 		
 
 	}
