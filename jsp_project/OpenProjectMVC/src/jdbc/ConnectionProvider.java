c5f3d524260add95a1d9a762883429b9449ac815
hxnky <71997918+hxnky@users.noreply.github.com>
2021-01-04 18:11:10 +0900

    210104-ajax

--- jsp_project/OpenProjectMVC/src/jdbc/ConnectionProvider.java
+++ jsp_project/OpenProjectMVC/src/jdbc/ConnectionProvider.java
@@ -11,9 +11,9 @@ public class ConnectionProvider {
 		Connection conn = null;
 
 		// 2. DB 연결 : Connection 객체를 얻어온다.
-		String jdbcUrl = "jdbc:mysql://localhost:3306/open?serverTimezone=UTC";
-		String user = "aia";
-		String password = "aia";
+		String jdbcUrl = "jdbc:mysql://openproject.cxefi7doaxtm.ap-northeast-2.rds.amazonaws.com:3306/open?serverTimezone=UTC";
+		String user = "hxnky";
+		String password = "1026";
 
 		conn = DriverManager.getConnection(jdbcUrl, user, password);
 
